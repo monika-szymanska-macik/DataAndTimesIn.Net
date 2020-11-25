74f0b644d3afbc811167abc52a19bb633b72aaf0
Monika <m.szymanska.macik@gmail.com>
2020-11-25 06:48:38 +0100

    timeSpan, addSeconds, Multiply

---
+++ DataAndTimes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace DataAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeSpan = new TimeSpan(60, 100, 200);
+            Console.WriteLine(timeSpan.Days);
+            Console.WriteLine(timeSpan.Hours);
+            Console.WriteLine(timeSpan.TotalMilliseconds);
+
+            var start = DateTimeOffset.UtcNow;
+            var end = start.AddSeconds(45);
+
+            TimeSpan difference = end - start;
+            Console.WriteLine(difference.Seconds);
+
+            difference = difference.Multiply(2);
+        }
+    }
+}
